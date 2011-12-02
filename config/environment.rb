21bd4ed351c8a44ad68621d1fda5dd9df6249989
Christian Joly <xianrecords@gmail.com>
2011-12-01 19:15:47 -0800

    fixed field_error issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Zadzip::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag
+end
